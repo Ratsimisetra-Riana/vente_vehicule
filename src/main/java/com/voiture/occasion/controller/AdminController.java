92a3c1846356967e00b3618b7bc052fe22f2b6a7
Ratsimisetra-Riana <119068522+Ratsimisetra-Riana@users.noreply.github.com>
2024-02-09 22:42:47 +0300

    Add files via upload

--- src/main/java/com/voiture/occasion/controller/AdminController.java
+++ src/main/java/com/voiture/occasion/controller/AdminController.java
@@ -27,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "*")
 @RestController
