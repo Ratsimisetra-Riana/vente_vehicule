6d0e30e95f28fd963b2332ea9039184ddfc5b760
Ratsimisetra-Riana <119068522+Ratsimisetra-Riana@users.noreply.github.com>
2024-02-10 22:15:23 +0300

    Update Marque.java

--- src/main/java/com/voiture/occasion/model/Marque.java
+++ src/main/java/com/voiture/occasion/model/Marque.java
@@ -8,6 +8,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 @Entity
